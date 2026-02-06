d78e553302d3a42b994f8d8152ffb0dbdc7d4c78
seryi2035 <lordon2035@gmail.com>
2026-02-07 00:36:46 +0300

    first hello world

---
+++ .qtcreator/qtc-cmake-presets-RDEvVrcy/Debug/build/CMakeFiles/4.1.4/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.12.63-gentoo-dist")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.12.63-gentoo-dist")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/media/flexshit/99gentoo/012STM32/proj007stm32c6t6CMAKE/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build/bluepillFSname-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,8 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "proj007stm32c6t6CMAKE.map"
+  )
+endif()
